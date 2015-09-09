7f3488b256723899dc9daa7592b0667d93918da0
Roy Gaya Espinoza <roy.gaya@me.com>
2015-09-09 16:43:58 +0100

    Revision three

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static('www'));
 
 app.get('/', function(req, res) {
-  res.send('contact');
+  res.send('index');
 });
 
 var server = app.listen(3000, function() {
